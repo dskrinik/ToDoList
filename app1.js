e612c3a28dea15de060a81f4a654f1b9603ab74b
David Skrinik <yskrinik@gmail.com>
2017-12-27 17:23:36 -0500

    jguery1

--- app1.js
+++ app1.js
@@ -1,5 +1,5 @@
-const ADDB = document.querySelector('.addButton');
-const TEXTF = document.querySelector('.textBox');
+var ADDB = document.querySelector('.addButton');
+var TEXTF = document.querySelector('.textBox');
 
 function editTask(element) {
   //if edit is clicked then uncheck the checkbox
@@ -8,6 +8,7 @@ function editTask(element) {
   if (checkBox.checked)
     checkBox.checked = false;
   crossOutIfCheked(p.childNodes[1]);
+
   //now edit the task
   p.cells[0].getElementsByTagName('label')[0].setAttribute('contenteditable', 'true');
   p.cells[0].getElementsByTagName('label')[0].focus();
@@ -25,7 +26,7 @@ function deleteTask(element) {
 }
 
 //checkbox crosses the taskName
-function crossOutIfCheked(element) {
+var crossOutIfCheked = function (element) {
   var p = element.parentNode;
   var checkBox = p.querySelector('.checkBox');
   if (checkBox.checked) {
@@ -33,66 +34,68 @@ function crossOutIfCheked(element) {
   } else {
     p.getElementsByTagName('label')[0].removeAttribute('style');
   }
-}
+};
 
 //addbutton creating and adding new tasks
 ADDB.addEventListener('click',
-      function (e) {
-        addTaskButton(e, this)
-      },
-      false);
-//crete space for a new task and add it to the table via the ADDbutton
- function addTaskButton(e, current) {
-   e.preventDefault();
-   current = TEXTF.value;
-   if (current == "") {
-     alert("Pease enter a task");
-   } else {
-     var table = document.getElementById("table1");
-     var row = table.insertRow(0);
-     var c1 = row.insertCell(0);
-     var c2 = row.insertCell(1);
-     var c3 = row.insertCell(2);
-     var newInputAndLabel = ('<input type="checkbox" class="checkBox" value="false"' +
-       'onchange= "crossOutIfCheked(this);"><label onfocusout = "outOfFocus(this);">') +
-       current + ('</label></div>');
-     var newEdit = ('<input type="button" value="edit" class="editButton" onclick="editTask(this);">');
-     var newDelete = ('<input type="button" value="delete" class="deleteButton" onclick="deleteTask(this)">');
-     c1.innerHTML = newInputAndLabel;
-     c2.innerHTML = newEdit;
-     c3.innerHTML = newDelete;
-     TEXTF.value = "";
-   }
- }
+  function (e) {
+    addTaskButton(e, this);
+  }, false);
 
- //textfield adds a new taks
- TEXTF.addEventListener('keyup',
-       function(e) {
-           addTask(e, this);
-       },
-       false);
+//crete space for a new task and add it to the table via the ADDbutton
+function addTaskButton(e, current) {
+  e.preventDefault();
+  current = TEXTF.value;
+  if (current == '') {
+    alert('Pease enter a task');
+  } else {
+    var table = document.getElementById('table1');
+    var row = table.insertRow(0);
+    var c1 = row.insertCell(0);
+    var c2 = row.insertCell(1);
+    var c3 = row.insertCell(2);
+    var newInputAndLabel = ('<input type="checkbox" class="checkBox" value="false"' +
+        'onchange= "crossOutIfCheked(this);"><label onfocusout = "outOfFocus(this);">') +
+      current + ('</label></div>');
+    var newEdit = ('<input type="button" value="edit" class="editButton" onclick="editTask(this);">');
+    var newDelete = ('<input type="button" value="delete" class="deleteButton" onclick="deleteTask(this)">');
+    c1.innerHTML = newInputAndLabel;
+    c2.innerHTML = newEdit;
+    c3.innerHTML = newDelete;
+    TEXTF.value = '';
+  }
+}
+
+//textfield adds a new taks
+TEXTF.addEventListener('keyup',
+  function (e) {
+    addTask(e, this);
+  },
+
+  false);
+
 //create space for a new task and add it to the table via the text field
 function addTask(e, current) {
   e.preventDefault();
   current = TEXTF.value;
-  if (current == "") {
-    alert("Pease enter a task");
+  if (current == '') {
+    alert('Pease enter a task');
   } else {
-    if (e.keyCode === 13) {//this is the diff from the addbutton
-      var table = document.getElementById("table1");
+    if (e.keyCode === 13) { //this is the diff from the addbutton
+      var table = document.getElementById('table1');
       var row = table.insertRow(0);
       var c1 = row.insertCell(0);
       var c2 = row.insertCell(1);
       var c3 = row.insertCell(2);
       var newInputAndLabel = ('<input type="checkbox" class="checkBox" value="false"' +
-        'onchange= "crossOutIfCheked(this);"><label onfocusout = "outOfFocus(this);">') +
+          'onchange= "crossOutIfCheked(this);"><label onfocusout = "outOfFocus(this);">') +
         current + ('</label></div>');
       var newEdit = ('<input type="button" value="edit" class="editButton" onclick="editTask(this);">');
       var newDelete = ('<input type="button" value="delete" class="deleteButton" onclick="deleteTask(this)">');
       c1.innerHTML = newInputAndLabel;
       c2.innerHTML = newEdit;
       c3.innerHTML = newDelete;
-      TEXTF.value = "";
+      TEXTF.value = '';
     }
   }
 }
